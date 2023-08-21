45a1c0055f8e58a9adbd6e11f006b67206f6680e
Vincent MARTOWICZ <vmartowicz@gmail.com>
2023-08-21 20:35:44 +0200

    Initial version of jhimsnodb generated by generator-jhipster@7.9.3

---
+++ src/test/java/com/mycompany/myapp/TechnicalStructureTest.java
@@ -0,0 +1,34 @@
+package com.mycompany.myapp;
+
+import static com.tngtech.archunit.base.DescribedPredicate.alwaysTrue;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.belongToAnyOf;
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+@AnalyzeClasses(packagesOf = JhimsnodbApp.class, importOptions = DoNotIncludeTests.class)
+class TechnicalStructureTest {
+
+    // prettier-ignore
+    @ArchTest
+    static final ArchRule respectsTechnicalArchitectureLayers = layeredArchitecture()
+        .layer("Config").definedBy("..config..")
+        .layer("Client").definedBy("..client..")
+        .layer("Web").definedBy("..web..")
+        .optionalLayer("Service").definedBy("..service..")
+        .layer("Security").definedBy("..security..")
+
+        .whereLayer("Config").mayNotBeAccessedByAnyLayer()
+        .whereLayer("Client").mayNotBeAccessedByAnyLayer()
+        .whereLayer("Web").mayOnlyBeAccessedByLayers("Config")
+        .whereLayer("Service").mayOnlyBeAccessedByLayers("Web", "Config")
+        .whereLayer("Security").mayOnlyBeAccessedByLayers("Config", "Client", "Service", "Web")
+
+        .ignoreDependency(belongToAnyOf(JhimsnodbApp.class), alwaysTrue())
+        .ignoreDependency(alwaysTrue(), belongToAnyOf(
+            com.mycompany.myapp.config.ApplicationProperties.class
+        ));
+}
